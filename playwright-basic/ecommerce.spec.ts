58572e9e20456f7e98f3782fe381adf22c8d3ab0
kseniaksyy <kseniia.orliuk@nixs.com>
2026-04-29 19:45:20 +0300

    Update ecommerce.spec.ts
    working with cart and removing item

--- playwright-basic/ecommerce.spec.ts
+++ playwright-basic/ecommerce.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('test', async ({ page }) => {
+  //navigate to
   await page.goto('https://www.saucedemo.com')
   await expect(page).toHaveURL('https://www.saucedemo.com')
   await expect(page.getByText('Swag Labs')).toBeVisible()
@@ -10,31 +11,38 @@ test('test', async ({ page }) => {
   await page.getByRole('button', { name: 'Login' }).click()
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
   await expect(page.getByText('Products')).toBeVisible()
-
   //sort products
   await page.getByText('Name (A to Z)Name (A to Z)').click()
   await page
     .locator('[data-test="product-sort-container"]')
     .selectOption('hilo')
-
   const firstItem = page.locator('.inventory_item').first()
-  await expect(firstItem).toContainText('Sauce Labs Fleece Jacket') // треба переробити і дійти до назви айтему та прайсу через "ланцюжок"
+  await expect(firstItem).toContainText('Sauce Labs Fleece Jacket')
   await expect(firstItem).toContainText('$49.99')
-
   //add to cart
   await page
     .locator('.inventory_item')
     .filter({ hasText: 'Sauce Labs Fleece Jacket' })
     .getByRole('button', { name: 'Add to cart' })
     .click()
-
   await expect(
     page
       .locator('.inventory_item')
       .filter({ hasText: 'Sauce Labs Fleece Jacket' })
       .getByRole('button', { name: 'Remove' }),
   ).toBeVisible()
-
   const cartBadge = page.locator('.shopping_cart_badge')
   await expect(cartBadge).toHaveText('1')
+
+  //workong with cart
+  await page.locator('[data-test="shopping-cart-link"]').click()
+  await expect(page).toHaveURL('https://www.saucedemo.com/cart.html')
+  const cartItem = page
+    .locator('.cart_item_label')
+    .filter({ hasText: 'Sauce Labs Fleece Jacket' })
+
+  //removing
+  await cartItem.getByRole('button', { name: 'Remove' }).click()
+  await expect(cartItem).toHaveCount(0)
+  await expect(cartBadge).toHaveCount(0)
 })
